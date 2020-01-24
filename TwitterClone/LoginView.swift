599dc93ab54c81a3b4a77807732b3bf4d3d7fed5
Masasi <chuanqiy6@gmail.com>
2020-01-23 17:23:17 -0800

    project up

--- TwitterClone/LoginView.swift
+++ TwitterClone/LoginView.swift
@@ -9,8 +9,50 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var mailaddress: String = ""
+    @State var password: String = ""
+    @State var username: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+        ZStack {
+            Color.red.edgesIgnoringSafeArea(.all)
+            VStack{
+//                Spacer()
+                Image("コンサドーレ3")
+                .resizable()
+
+                .frame(width:400, height: 300)
+//                    .background(Color.red)
+//                Spacer()
+                Text("⚽️ログイン情報⚽️")
+                .fontWeight(.heavy)
+                .font(.title)
+                TextField("メールアドレス", text: $mailaddress)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+//                Text(mailaddress)
+                SecureField("パスワード", text:  $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+//                Text(password)
+//                Spacer()
+                NavigationLink(destination: SignupView()){
+                Text("新規登録はこちら")
+                    .foregroundColor(.white)
+                    .fontWeight(.heavy)
+                    .font(.title)
+                    .frame(width:330, height: 80)
+                    .background(Color.black)
+                    .cornerRadius(12)
+                    .offset(y: 20)
+                }.navigationBarTitle(Text("ログイン"))
+                Spacer()
+                
+            }
+        }
+        }
     }
 }
 
--- TwitterClone/SignupView.swift
+++ TwitterClone/SignupView.swift
@@ -9,13 +9,44 @@
 import SwiftUI
 
 struct SignupView: View {
+    @State var mailaddress: String = ""
+    @State var password: String = ""
+    @State var username: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.red.edgesIgnoringSafeArea(.all)
+            VStack{
+            Image("コンサドーレ5")
+                Text("⚽️新規登録⚽️")
+                    .fontWeight(.heavy)
+                    .font(.title)
+                TextField("お名前", text: $username)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                TextField("メールアドレス", text: $mailaddress)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                //                Text(mailaddress)
+                SecureField("パスワード", text:  $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                Text("登録完了！")
+                    .foregroundColor(.white)
+                    .fontWeight(.heavy)
+                    .font(.title)
+                    .frame(width:330, height: 80)
+                    .background(Color.black)
+                    .cornerRadius(12)
+                    .offset(y:20)
+                Spacer()
+                
+            }
+        }
     }
 }
 
-struct SignupView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignupView()
-    }
-}
+//struct SignupView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SignupView()
+//    }
+//}
